5278e87286e8b431970bfd4ebd8b0642f984d4a2
Ellsworth McCullough <ellsworthmmcc@outlook.com>
2026-04-01 13:19:31 -0700

    feat: simple implementation of bresenham's line alg

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+/*
+To compile on windows:
+cmake -Bbuild && cmake --build build -j && build\Debug\tinyrenderer obj/diablo3_pose/diablo3_pose.obj obj/floor.obj
+*/
 #include <cmath>
 #include "tgaimage.h"
 
@@ -7,6 +11,14 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color) {
+  for (float t = 0.; t < 1.; t += .02) {
+    int x = std::round(ax + (bx - ax) * t);
+    int y = std::round(ay + (by - ay) * t);
+    framebuffer.set(x, y, color);
+  }
+}
+
 int main(int argc, char** argv) {
     constexpr int width  = 64;
     constexpr int height = 64;
@@ -15,6 +27,11 @@ int main(int argc, char** argv) {
     int ax =  7, ay =  3;
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
+
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
