e993804fa7c79a2f8401d73b8ae72ee4a8ca54ed
Ellsworth McCullough <ellsworthmmcc@outlook.com>
2026-04-20 23:23:14 -0700

    feat: color used to show depth in bounding box calculation

--- main.cpp
+++ main.cpp
@@ -139,31 +139,73 @@ double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
   return .5 * ((by - ay) * (bx + ax) + (cy - by) * (cx + bx) + (ay - cy) * (ax + cx));
 }
 
-void triangle_bounding_box(int ax, int ay, int bx, int by, int cx, int cy, TGAImage& framebuffer, TGAColor color) {
+template<typename PixelShader>
+requires std::invocable<PixelShader, int, int, double, double, double>
+void rasterize_triangle(int ax, int ay, int bx, int by, int cx, int cy, double total_area, PixelShader pixel_shader) {
   int bbminx = std::min(std::min(ax, bx), cx); // bounding box for the triangle
   int bbminy = std::min(std::min(ay, by), cy); // defined by its top left and bottom right corners
   int bbmaxx = std::max(std::max(ax, bx), cx);
   int bbmaxy = std::max(std::max(ay, by), cy);
-  double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
-  if (total_area < 1) return; // backface culling + discarding triangles that cover less than a pixel
 
-  #pragma omp parallel for
+#pragma omp parallel for
   for (int x = bbminx; x <= bbmaxx; x++) {
     for (int y = bbminy; y <= bbmaxy; y++) {
       double alpha = signed_triangle_area(x, y, bx, by, cx, cy) / total_area;
       double beta = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
       double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
       if (alpha < 0 || beta < 0 || gamma < 0) continue; // negative barycentric coordinate => the pixel is outside the triangle
-      framebuffer.set(x, y, color);
+      pixel_shader(x, y, alpha, beta, gamma);
     }
   }
 }
 
-int triangle_testing() {
+void triangle_bounding_box(int ax, int ay, int bx, int by, int cx, int cy, TGAImage& framebuffer, TGAColor color, bool cull = true) {
+  double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
+  if (cull) {
+    if (total_area < 1) return; // backface culling + discarding triangles that cover less than a pixel
+  }
+
+  rasterize_triangle(ax, ay, bx, by, cx, cy, total_area,
+    [&](int x, int y, double, double, double) {
+      framebuffer.set(x, y, color);
+    }
+  );
+}
+
+void triangle_bounding_box(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, int cz, TGAImage& framebuffer, bool cull = true) {
+  double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
+  if (cull) {
+    if (total_area < 1) return; // backface culling + discarding triangles that cover less than a pixel
+  }
+
+  rasterize_triangle(ax, ay, bx, by, cx, cy, total_area,
+    [&](int x, int y, double alpha, double beta, double gamma) {
+      unsigned char z = static_cast<unsigned char>(alpha * az + beta * bz + gamma * cz);
+      framebuffer.set(x, y, { z });
+    }
+  );
+}
+
+int triangle_bounding_testing() {
   TGAImage framebuffer(width, height, TGAImage::RGB);
-  triangle_bounding_box(7, 45, 35, 100, 45, 60, framebuffer, red);
-  triangle_bounding_box(120, 35, 90, 5, 45, 110, framebuffer, white);
-  triangle_bounding_box(115, 83, 80, 90, 85, 120, framebuffer, green);
+  triangle_bounding_box(7, 45, 35, 100, 45, 60, framebuffer, red, false);
+  triangle_bounding_box(120, 35, 90, 5, 45, 110, framebuffer, white, false);
+  triangle_bounding_box(115, 83, 80, 90, 85, 120, framebuffer, green, false);
+  framebuffer.write_tga_file("framebuffer.tga");
+  return 0;
+}
+
+int triangle_bounding_testing_depth() {
+  constexpr int width = 64;
+  constexpr int height = 64;
+  TGAImage framebuffer(width, height, TGAImage::GRAYSCALE);
+
+  int ax = 17, ay = 4, az = 13;
+  int bx = 55, by = 39, bz = 128;
+  int cx = 23, cy = 59, cz = 255;
+
+  triangle_bounding_box(ax, ay, az, bx, by, bz, cx, cy, cz, framebuffer);
+
   framebuffer.write_tga_file("framebuffer.tga");
   return 0;
 }
@@ -194,6 +236,7 @@ int generate_triangle_rasterization(int argc, char** argv) {
 int main(int argc, char** argv) {
   // line_testing();
   // generate_wireframe(argc, argv);
-  // triangle_testing();
+  // triangle_bounding_testing();
+  triangle_bounding_testing_depth();
   // generate_triangle_rasterization(argc, argv);
 }
